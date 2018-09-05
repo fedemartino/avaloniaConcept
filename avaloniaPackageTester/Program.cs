d123df3db239ad013e158c16b3658e569c83e354
fedemartino <fedemartino@gmail.com>
2018-09-05 01:42:47 -0300

    ordenar archivos

---
+++ avaloniaPackageTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using AvaloniaTest.ViewModels;
+using AvaloniaTest.Views;
+
+namespace avaloniaPackageTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BuildAvaloniaApp().Start<MainWindow>(() => new AvaloniaTest.ViewModels.MainWindowViewModel());
+        }
+
+        
+    }
+}
